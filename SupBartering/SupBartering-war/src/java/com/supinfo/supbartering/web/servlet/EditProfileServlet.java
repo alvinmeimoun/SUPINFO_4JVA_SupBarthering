551b4e60a9c74e666c896278c7db4a6216d2b297
Alvin Meimoun <alvin.meimoun@gmail.com>
2015-12-18 11:31:34 +0100

    feat(user): Edit my profile

--- SupBartering/SupBartering-war/src/java/com/supinfo/supbartering/web/servlet/EditProfileServlet.java
+++ SupBartering/SupBartering-war/src/java/com/supinfo/supbartering/web/servlet/EditProfileServlet.java
@@ -5,14 +5,27 @@
  */
 package com.supinfo.supbartering.web.servlet;
 
+import com.supinfo.supbartering.ejb.business.UserManager;
+import com.supinfo.supbartering.ejb.entity.UserEntity;
+import com.supinfo.supbartering.ejb.facade.UserFacade;
+import com.supinfo.supbartering.web.utils.UserAuthenticationUtils;
 import java.io.IOException;
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.codec.digest.DigestUtils;
 
 public class EditProfileServlet extends HttpServlet {
 
+    @EJB
+    UserManager userManager;
+    
+    @EJB
+    UserFacade userFacade;
+    
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -38,6 +51,35 @@ public class EditProfileServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String cookieUsername = UserAuthenticationUtils.GetConnectedUser(request);
+        
+        UserEntity user = userFacade.findByUsername(cookieUsername);
+        
+        user.setEmail(request.getParameter("email"));
+        user.setFirstName(request.getParameter("firstname"));
+        user.setLastName(request.getParameter("lastname"));
+        
+        String oldPassword = request.getParameter("oldPassword");
+        String newPassword = request.getParameter("newPassword");
+        String confirmNewPassword = request.getParameter("confirmNewPassword");
+        
+        if(oldPassword != null && newPassword != null && confirmNewPassword != null
+                && oldPassword.length() > 0 && newPassword.length() > 0 &&
+                confirmNewPassword.length() > 0){
+            //Changement de mot de passe
+            if(user.getPassword().equals(DigestUtils.sha256Hex(oldPassword))){
+                //Ancien mot de passe valide
+                if(newPassword.equals(confirmNewPassword)){
+                    //Confirmation du nouveua mot de passe valide
+                    user.setPassword(DigestUtils.sha256Hex(newPassword));
+                }
+            }
+        }
+        
+        userManager.addOrUpdateUser(user);
+        
+        RequestDispatcher rd = request.getRequestDispatcher("login");
+        rd.forward(request, response);
     }
 
     /**
@@ -47,7 +89,7 @@ public class EditProfileServlet extends HttpServlet {
      */
     @Override
     public String getServletInfo() {
-        return "Short description";
+        return "Edit my profile";
     }
 
 }
